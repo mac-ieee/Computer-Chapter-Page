419e6d8f6430020ac81910834a60b59f4bee56fb
qalvinsuen <qalvinsuen@hotmail.com>
2021-08-15 01:38:16 -0400

    Updates

--- src/components/IFrame.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function IFrame(props){
-    return (
-        <div dangerouslySetInnerHTML={ {__html:  props.iframe?props.iframe:""}} />
-    );
-}
-
-export default IFrame
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import ReactRouterSetup from "./ReactRouterSetup"
-
-ReactDOM.render(<ReactRouterSetup/>, document.getElementById("root"))
-
